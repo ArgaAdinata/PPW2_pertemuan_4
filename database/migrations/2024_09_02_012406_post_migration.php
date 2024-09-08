6310b63291af992a6c683aedf3b5464441e21e0d
Arga Adinata <argaadinata10@gmail.com>
2024-09-08 22:38:15 +0700

    first commit

---
+++ database/migrations/2024_09_02_012406_post_migration.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+        });
+
+        Schema::create('tugas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); 
+            $table->string('name');
+            $table->text('description');
+            $table->integer('age');
+            $table->boolean('is_active');
+            $table->decimal('price', 8, 2);
+            $table->float('rating', 8, 2);
+            $table->enum('status', ['pending', 'approved', 'rejected']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+            
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
